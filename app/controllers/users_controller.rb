ee83645e59f4a0d2f22936edd9869cc6a54de121
EC2 Default User <ec2-user@ip-172-31-33-102.us-east-2.compute.internal>
2019-10-28 13:44:27 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+  def show
+    # @user = User.first
+    @user = User.find(params[:id])
+    # debugger
+    # app/views/users/show.html.erb
+  end  
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      #success
+      flash[:success] = "flash.eachのkey :success value: welcome to ~~~~"
+      redirect_to @user
+    else
+      #failure
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
   end
 end
