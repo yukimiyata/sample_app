7cab934c52509bd1375cc77c4ee0e40c90a86961
EC2 Default User <ec2-user@ip-172-31-33-102.us-east-2.compute.internal>
2019-10-30 23:35:32 +0000

    Finish ch10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,10 +21,6 @@ module SessionsHelper
     !current_user.nil?
   end
   
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
   
   # ユーザーのセッションを永続的にする
   def remember(user)
@@ -45,5 +41,21 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
