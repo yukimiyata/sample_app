5c26723f43a84b188b5c7e1439d7048ed9fdb0d0
EC2 Default User <ec2-user@ip-172-31-33-102.us-east-2.compute.internal>
2019-10-31 15:58:35 +0000

    Finish ch12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
   root 'static_pages#home'
@@ -12,4 +16,5 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, onky:[:new, :create, :edit, :update]
 end
