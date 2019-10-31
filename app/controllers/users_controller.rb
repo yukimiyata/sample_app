5bb77c68cb1a63ccbacedb6b93da5928dc79b172
EC2 Default User <ec2-user@ip-172-31-33-102.us-east-2.compute.internal>
2019-10-31 11:52:32 +0000

    Finish ch11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,14 +20,11 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    
     if @user.save
-      #success
-      log_in @user
-      flash[:success] = "flash.eachのkey :success value: welcome to ~~~~"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
-      #failure
       render 'new'
     end
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.delivery_method = :test
+  host = '051910937df041bfa7d2752b04621d4b.vfs.cloud9.us-east-2.amazonaws.com' # ここをコピペすると失敗します。自分の環境に合わせてください。
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
